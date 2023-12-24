5f7d4ab9da3960fb7eb8d9916f9a2e4abd862cc0
kgalvin <kgalvin@nvidia.com>
2023-12-24 09:42:03 -0800

    update headers for sharedArrayBuffer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import Head from '@docusaurus/Head';
 
 import styles from './index.module.css';
 
@@ -27,6 +28,10 @@ export default function Home(): JSX.Element {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
+      <Head>
+        <meta http-equiv="Cross-Origin-Embedder-Policy" content="require-corp"/>
+        <meta http-equiv="Cross-Origin-Opener-Policy" content="same-origin"/>
+      </Head>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
